2107362b7dc359c164c0899b82667d02a733f5a2
Wouter Jansen <wouter.jansen@student.uantwerpen.be>
2017-02-24 20:46:23 +0100

    reconnect option added. Filtering song re-added.

--- app/src/main/java/be/uantwerpen/ds/iot/musicratingbox/MusicRating.java
+++ app/src/main/java/be/uantwerpen/ds/iot/musicratingbox/MusicRating.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -25,12 +26,15 @@ public class MusicRating extends AppCompatActivity implements MqttCallback {
     TextView songTitleTextView;
     TextView songArtistTextView;
     TextView songAlbumTextView;
-    TextView songYearTextView;
-    MqttAndroidClient mqttAndroidClient;
+    TextView reconnectTextView;
+    Button reconnectButton;
     final String serverUri = "tcp://broker.hivemq.com:1883";
     String clientId = MqttClient.generateClientId();
     final String subscriptionTopic = "songInformation";
     final String publishTopic = "songVote";
+    MqttAndroidClient mqttAndroidClient;
+    String songID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,48 +42,57 @@ public class MusicRating extends AppCompatActivity implements MqttCallback {
         setContentView(R.layout.activity_music_rating);
         greenButton = (FloatingActionButton)findViewById(R.id.greenButton);
         redButton = (FloatingActionButton)findViewById(R.id.redButton);
+        reconnectButton = (Button)findViewById(R.id.reconnectButton);
+        reconnectTextView = (TextView)findViewById(R.id.reconnectTextView);
+        disableButtons();
         songTitleTextView = (TextView)findViewById(R.id.songTitleTextView);
         songArtistTextView = (TextView)findViewById(R.id.songArtistTextView);
         songAlbumTextView = (TextView)findViewById(R.id.songAlbumTextView);
-        songYearTextView = (TextView)findViewById(R.id.songAlbumTextView);
         mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
         mqttAndroidClient.setCallback(MusicRating.this);
+        connectMQTT();
+    }
 
+    public void connectMQTT(){
         try {
             IMqttToken token = mqttAndroidClient.connect();
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
+                    enableText();
+                    enableButtons();
+                    reconnectButton.setVisibility(View.INVISIBLE);
+                    reconnectTextView.setVisibility(View.INVISIBLE);
                     subscribeToTopic();
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-
+                    Toast.makeText(MusicRating.this, "ERROR: Cannot connect", Toast.LENGTH_SHORT).show();
+                    reconnectButton.setVisibility(View.VISIBLE);
+                    reconnectTextView.setVisibility(View.VISIBLE);
                 }
             });
         } catch (MqttException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public void reconnectMQTT(View v){
+        connectMQTT();
     }
 
     public void onClickGreen(View v) {
         Toast.makeText(MusicRating.this, "You have upvoted this song!", Toast.LENGTH_SHORT).show();
-        publishMessage("upvoted");
-        songArtistTextView.setVisibility(View.VISIBLE);
-        songAlbumTextView.setVisibility(View.VISIBLE);
-        songYearTextView.setVisibility(View.VISIBLE);
-        disableButtons();
+        publishMessage(songID + "1");
+        timeoutButtons(5000);
     }
 
     public void onClickRed(View v) {
         Toast.makeText(MusicRating.this, "You have downvoted this song!", Toast.LENGTH_SHORT).show();
-        publishMessage("downvoted");
-        songArtistTextView.setVisibility(View.VISIBLE);
-        songAlbumTextView.setVisibility(View.VISIBLE);
-        songYearTextView.setVisibility(View.VISIBLE);
-        disableButtons();
+        publishMessage(songID + "0");
+        timeoutButtons(5000);
 
     }
 
@@ -92,6 +105,7 @@ public class MusicRating extends AppCompatActivity implements MqttCallback {
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MusicRating.this, "ERROR: Cannot connect(subscription)", Toast.LENGTH_SHORT).show();
                 }
             });
         } catch (MqttException ex){
@@ -108,6 +122,7 @@ public class MusicRating extends AppCompatActivity implements MqttCallback {
             mqttAndroidClient.publish(publishTopic, message);
         } catch (MqttException e) {
             System.err.println("Error Publishing: " + e.getMessage());
+            Toast.makeText(MusicRating.this, "ERROR: Cannot connect(publish)", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
@@ -119,34 +134,65 @@ public class MusicRating extends AppCompatActivity implements MqttCallback {
 
     @Override
     public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-        Toast.makeText(MusicRating.this, "The diskjockey is playing a new song", Toast.LENGTH_SHORT).show();
-        songArtistTextView.setVisibility(View.VISIBLE);
-        songAlbumTextView.setVisibility(View.VISIBLE);
-        songYearTextView.setVisibility(View.VISIBLE);
+        enableButtons();
+        setSongInformation(mqttMessage);
+    }
+
+    private void setSongInformation(MqttMessage mqttMessage){
+        String[] songParts = mqttMessage.toString().split("#%");
+        songTitleTextView.setText(songParts[1]);
+        if(songParts[2] != ""){
+            songArtistTextView.setText(songParts[2]);
+        }else{
+            songArtistTextView.setText("");
+        }
+        if(songParts[3] != ""){
+            Toast.makeText(MusicRating.this, songParts[4], Toast.LENGTH_SHORT).show();
+            if(songParts[4] != ""){
+                songAlbumTextView.setText(songParts[3] + "(" + songParts[4] + ")");
+            }else{
+                songAlbumTextView.setText(songParts[3]);
+            }
+
+        }else {
+            songAlbumTextView.setText("");
+        }
+        songID = songParts[0];
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
 
     }
+    public void timeoutButtons(int timeout){
+        disableButtons();
+        greenButton.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                enableButtons();
+            }
+        }, timeout);
+    }
+
+    public void enableText(){
+        songTitleTextView.setVisibility(View.VISIBLE);
+        songArtistTextView.setVisibility(View.VISIBLE);
+        songAlbumTextView.setVisibility(View.VISIBLE);
+    }
+
     public void disableButtons(){
         greenButton.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
         redButton.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
         greenButton.setEnabled(false);
         redButton.setEnabled(false);
-        greenButton.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                greenButton.setEnabled(true);
-                greenButton.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(36,163,8)));
-            }
-        }, 5000);
-        redButton.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                redButton.setEnabled(true);
-                redButton.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(163,8,8)));
-            }
-        }, 5000);
+    }
+
+    public void enableButtons(){
+        greenButton.setVisibility(View.VISIBLE);
+        redButton.setVisibility(View.VISIBLE);
+        greenButton.setEnabled(true);
+        redButton.setEnabled(true);
+        greenButton.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(36,163,8)));
+        redButton.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(163,8,8)));
     }
 }
