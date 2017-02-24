cc6788ddacf077b617b0620c4e1d60c7ee695d01
Gebruiker <nick_de_clerck@hotmail.com>
2017-02-24 14:35:29 +0100

    MQTT publish/subscribe fixed

--- app/src/main/java/be/uantwerpen/ds/iot/musicratingbox/MusicRating.java
+++ app/src/main/java/be/uantwerpen/ds/iot/musicratingbox/MusicRating.java
@@ -2,21 +2,26 @@ package be.uantwerpen.ds.iot.musicratingbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class MusicRating extends AppCompatActivity {
+public class MusicRating extends AppCompatActivity implements MqttCallback {
 
     TextView songTitleTextView;
+    TextView songArtistTextView;
+    TextView songAlbumTextView;
     MqttAndroidClient mqttAndroidClient;
-    final String serverUri = "tcp://test.mosquitto.org:1883";
+    final String serverUri = "tcp://broker.hivemq.com:1883";
     String clientId = MqttClient.generateClientId();
     final String subscriptionTopic = "songInformation";
     final String publishTopic = "songVote";
@@ -26,8 +31,11 @@ public class MusicRating extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_rating);
         songTitleTextView = (TextView)findViewById(R.id.songTitleTextView);
+        songArtistTextView = (TextView)findViewById(R.id.songArtistTextView);
+        songAlbumTextView = (TextView)findViewById(R.id.songAlbumTextView);
 
         mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(MusicRating.this);
 
         try {
             IMqttToken token = mqttAndroidClient.connect();
@@ -50,13 +58,11 @@ public class MusicRating extends AppCompatActivity {
 
     public void onClickGreen(View v) {
         Toast.makeText(MusicRating.this, "You have upvoted this song!", Toast.LENGTH_SHORT).show();
-        songTitleTextView.setText("Upvote");
         publishMessage("upvoted");
     }
 
     public void onClickRed(View v) {
         Toast.makeText(MusicRating.this, "You have downvoted this song!", Toast.LENGTH_SHORT).show();
-        songTitleTextView.setText("Downvote");
         publishMessage("downvoted");
     }
 
@@ -65,7 +71,6 @@ public class MusicRating extends AppCompatActivity {
             mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    songTitleTextView.setText("song changed");
                 }
 
                 @Override
@@ -88,5 +93,20 @@ public class MusicRating extends AppCompatActivity {
             System.err.println("Error Publishing: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+        Toast.makeText(MusicRating.this, "The diskjockey is playing a new song", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
     }
 }
